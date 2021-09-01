aa5feeaf64cebea384829ff9f16607bdeadc873a
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2021-08-31 21:42:35 -0400

    Some changes to projectile

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -4,19 +4,23 @@ namespace Projectiles
 {
     public class Projectile : MonoBehaviour
     {
-        [SerializeField] private float speed;
-    
         private Rigidbody2D _myRigidbody;
+        private float _speed;
 
+        public float Speed
+        {
+            get => _speed;
+            set
+            {
+                _speed = value;
+                _myRigidbody.velocity = transform.up * value;
+            }
+        }
+        
         private void Awake()
         {
             _myRigidbody = GetComponent<Rigidbody2D>();
-            _myRigidbody.velocity = transform.up * speed;
-        }
-
-        public void SetSpeed(float newSpeed)
-        {
-            _myRigidbody.velocity = transform.up * newSpeed;
+            _myRigidbody.velocity = transform.up * _speed;
         }
     }
 }
--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -11,11 +11,13 @@ namespace Weapons
         [SerializeField] private Transform muzzle;
         [SerializeField] private float projectileSpeed = 5;
         [SerializeField] private float projectileDuration = 5;
+        [SerializeField] private float projectileScale = 0.4f;
 
         private void OnFire()
         {            
             Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation);
-            newProjectile.SetSpeed(projectileSpeed);
+            newProjectile.transform.localScale = Vector3.one * projectileScale;
+            newProjectile.Speed = projectileSpeed;
             newProjectile.transform.Translate(Vector3.up * newProjectile.transform.localScale.y / 2, Space.Self);
             
             Temporary projectileLifetime = newProjectile.gameObject.AddComponent<Temporary>();
