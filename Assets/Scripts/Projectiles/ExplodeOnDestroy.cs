bc720cbe84504c09cb7bd79a5d608198b6db18c0
JonathanThing <jonathancai834@gmail.com>
2021-09-01 14:53:57 -0400

    minor changes to shrapnel shot and added machine gun even tho its just a regular gun

--- Assets/Scripts/Projectiles/ExplodeOnDestroy.cs
+++ Assets/Scripts/Projectiles/ExplodeOnDestroy.cs
@@ -12,21 +12,21 @@ public class ExplodeOnDestroy : MonoBehaviour
     [SerializeField] private float projectileSpeed = 5;
     [SerializeField] private float projectileDuration = 5;
     [SerializeField] private int numberOfProjectiles = 5;
-    [SerializeField] private bool wallCollision = true;
+    [SerializeField] private int shrapnelType = 0;
     [SerializeField] private int speedVariance = 2;
+    [SerializeField] private int timeVariance = 5;
     
     private void OnDestroy() {
 
         for (int _ = 0; _ < numberOfProjectiles; _++)
         {
-            int randIndex = Random.Range(0, shrapnels.Length - 1);
-            
-            Projectile newProjectile = Instantiate(shrapnels[randIndex], transform.position, transform.rotation);
+
+            Projectile newProjectile = Instantiate(shrapnels[shrapnelType], transform.position, transform.rotation);
             newProjectile.transform.eulerAngles = Vector3.forward * Random.Range(0, 360);
             newProjectile.Speed = Random.Range(projectileSpeed-speedVariance, projectileSpeed +speedVariance);
             
             Temporary projectileLifetime = newProjectile.gameObject.AddComponent<Temporary>();
-            projectileLifetime.SetDuration(projectileDuration);
+            projectileLifetime.SetDuration(Random.Range(projectileDuration-timeVariance,projectileDuration+timeVariance));
         }
     }
 }
